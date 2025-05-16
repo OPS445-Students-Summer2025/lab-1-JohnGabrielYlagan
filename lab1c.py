8f2e085ff3244aedddf3c798fc30b97861ce42d2
John Gabriel Ylagan <jgylagan@myseneca.ca>
2025-05-16 09:37:55 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: John Gabriel Ylagan
+# lab1c
+
+'''Description: This program will use both integers and string in a print'''
+
+name = "Isaac"
+age = 72
+
+print(name + " is " + str(age) + " years old!")
