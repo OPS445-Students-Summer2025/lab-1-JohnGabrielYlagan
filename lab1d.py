128ee645ab55fa1e23cca226a0765d33396131e9
John Gabriel Ylagan <jgylagan@myseneca.ca>
2025-05-16 10:59:38 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Author: John Gabriel Ylagan
+# lab1d
+
+"""
+This script demonstrates operations and printing math with assigned variables.
+"""
+
+x = 10
+y = 2    # my assigned variables
+z = 5
+
+answer = x + y * z
+print(str(x) + " + " + str(y) + " * " + str(z) + " = " + str(answer))
