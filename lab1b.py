a51007a3cd3bd79eb58c470af63e1906686658d1
John Gabriel Ylagan <jgylagan@myseneca.ca>
2025-05-16 09:24:59 -0400

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Author: John Gabriel Ylagan
+# lab1b
+
+'''Description: This program will print a sentence including a name variable'''
+
+name =  'Isaac'
+print('How old are you ' + name + "?")
+
